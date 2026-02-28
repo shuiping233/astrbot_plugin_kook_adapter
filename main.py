f40908d2a8081ae905af3d8680ff0f4f7609c7dd
shuiping233 <1944680304@qq.com>
2026-03-01 00:02:52 +0800

    chore: 版本号修改到0.0.4

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from astrbot.api.star import Context, Star, register
 
 
-@register("kook_adapter", "wuyan1003", "KOOK适配器", "0.0.3")
+@register("kook_adapter", "wuyan1003", "KOOK适配器", "0.0.4")
 class KookAdapterPlugin(Star):
     def __init__(self, context: Context):
         from .kook_adapter.kook_adapter import KookPlatformAdapter  # noqa
