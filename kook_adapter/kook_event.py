b0b19837f76660006062e5b64b3d70ec032336ee
shuiping233 <1944680304@qq.com>
2026-03-01 08:27:10 +0800

    feat: 实现audio类型消息支持

--- kook_adapter/kook_event.py
+++ kook_adapter/kook_event.py
@@ -1,12 +1,20 @@
 import asyncio
 from collections.abc import Coroutine
+import json
+from pathlib import Path
 from typing import Any
 
 from astrbot import logger
 from astrbot.api.event import AstrMessageEvent, MessageChain
 from astrbot.api.message_components import File, Image, Plain, Video
 from astrbot.api.platform import AstrBotMessage, PlatformMetadata
-from astrbot.core.message.components import At, AtAll, BaseMessageComponent, Reply
+from astrbot.core.message.components import (
+    At,
+    AtAll,
+    BaseMessageComponent,
+    Record,
+    Reply,
+)
 
 from .kook_client import KookClient
 from .kook_types import (
@@ -30,7 +38,7 @@ class KookEvent(AstrMessageEvent):
         self._file_message_counter = 0
 
     def _warp_message(
-        self, index: int, messageComponent: BaseMessageComponent
+        self, index: int, message_component: BaseMessageComponent
     ) -> Coroutine[Any, Any, OrderMessage]:
         async def wrap_upload(
             index: int, message_type: KookMessageType, upload_coro
@@ -52,13 +60,13 @@ class KookEvent(AstrMessageEvent):
                 reply_id=reply_id,
             )
 
-        match messageComponent:
+        match message_component:
             case Image():
                 self._file_message_counter += 1
                 return wrap_upload(
                     index,
                     KookMessageType.IMAGE,
-                    self.client.upload_asset(messageComponent.file),
+                    self.client.upload_asset(message_component.file),
                 )
 
             case Video():
@@ -66,30 +74,59 @@ class KookEvent(AstrMessageEvent):
                 return wrap_upload(
                     index,
                     KookMessageType.VIDEO,
-                    self.client.upload_asset(messageComponent.file),
+                    self.client.upload_asset(message_component.file),
                 )
             case File():
 
-                async def handle_file(idx=index, f_item=messageComponent):
+                async def handle_file(index: int, f_item: File):
                     f_data = await f_item.get_file()
                     url = await self.client.upload_asset(f_data)
-                    return OrderMessage(index=idx, text=url, type=KookMessageType.FILE)
+                    return OrderMessage(
+                        index=index, text=url, type=KookMessageType.FILE
+                    )
 
                 self._file_message_counter += 1
-                return handle_file()
+                return handle_file(index, message_component)
+
+            case Record():
+
+                async def handle_audio(index: int, f_item: Record):
+                    file_path = await f_item.convert_to_file_path()
+                    url = await self.client.upload_asset(file_path)
+                    title = f_item.text or Path(file_path).name
+                    return OrderMessage(
+                        index=index,
+                        text=json.dumps(
+                            [
+                                {
+                                    "type": "card",
+                                    "modules": [
+                                        {
+                                            "type": "audio",
+                                            "title": title,
+                                            "src": url,
+                                        }
+                                    ],
+                                }
+                            ]
+                        ),
+                        type=KookMessageType.CARD,
+                    )
+
+                return handle_audio(index, message_component)
             case Plain():
-                return handle_plain(index, messageComponent.text)
+                return handle_plain(index, message_component.text)
             case At():
-                return handle_plain(index, f"(met){messageComponent.qq}(met)")
+                return handle_plain(index, f"(met){message_component.qq}(met)")
             case AtAll():
                 return handle_plain(index, "(met)all(met)")
             case Reply():
                 return handle_plain(
-                    index, messageComponent.text, reply_id=messageComponent.id
+                    index, message_component.text, reply_id=message_component.id
                 )
             case _:
                 raise NotImplementedError(
-                    f'kook适配器尚未实现对 "{messageComponent.type}" 消息类型的支持'
+                    f'kook适配器尚未实现对 "{message_component.type}" 消息类型的支持'
                 )
 
     async def send(self, message: MessageChain):
