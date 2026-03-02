9832abc34b0719989cd502bc5b9f78af43032efd
shuiping233 <1944680304@qq.com>
2026-03-02 11:39:54 +0800

    refactor: 移除废弃的astrbot插件装饰器

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-from astrbot.api.star import Context, Star, register
+from astrbot.api.star import Context, Star
 
 
-@register("kook_adapter", "wuyan1003", "KOOK适配器", "0.0.4")
 class KookAdapterPlugin(Star):
     def __init__(self, context: Context):
         from .kook_adapter.kook_adapter import KookPlatformAdapter  # noqa
